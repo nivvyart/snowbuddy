417754f81c30629e259daae30ff3713265f3f66f
Adrian Thorburn <adrian.thorburn@gmail.com>
2019-04-22 12:29:47 +1000

    db seeded

--- db/migrate/20190422010155_create_messages.rb
+++ db/migrate/20190422010155_create_messages.rb
@@ -1,6 +1,6 @@
 class CreateMessages < ActiveRecord::Migration[5.2]
   def change
-    create_table :messages do |t|
+    create_table :messages, :id => false do |t|
       t.integer :message_id
       t.integer :resort_id
       t.text :message
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,17 @@ User.destroy_all
 u1 = User.create :email => 'adrian@snowbuddy.com', :password => 'asdasd'
 u2 = User.create :email => 'nico@snowbuddy.com', :password => 'asdasd'
 
+p "user created"
+
 Resort.destroy_all
 r1 = Resort.create :name => 'Mount Hutt', :region => 'New Zealand', :homepage => 'https://www.mthutt.co.nz/', :lifts => 5, :elevation => '2190', :area => '365'
 r2 = Resort.create :name => 'Loveland', :region => 'Colorado', :homepage => 'http://www.skiloveland.com/', :lifts => 10, :elevation => '3965', :area => '1800'
 r3 = Resort.create :name => 'Arapahoe Basin', :region => 'Colorado', :homepage => 'http://arapahoebasin.com/', :lifts => 9, :elevation => '3978', :area => '1458'
 
-Messages.destroy_all
+p "resort created"
+
+Message.destroy_all
 m1 = Message.create :message => 'Looking for Buddy for A-Basin'
 m2 = Message.create :message => 'Looking for Buddy for Mt hutt'
+
+p "message created"
