fe22a58f00c413f9a474595888721d12e95f03ed
Adrian Thorburn <adrian.thorburn@gmail.com>
2019-04-22 11:02:14 +1000

    messages table setup

---
+++ db/migrate/20190422010155_create_messages.rb
@@ -0,0 +1,11 @@
+class CreateMessages < ActiveRecord::Migration[5.2]
+  def change
+    create_table :messages do |t|
+      t.integer :message_id
+      t.integer :resort_id
+      t.text :message
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_22_010029) do
+ActiveRecord::Schema.define(version: 2019_04_22_010155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "message_id"
+    t.integer "resort_id"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "resorts", force: :cascade do |t|
     t.text "name"
