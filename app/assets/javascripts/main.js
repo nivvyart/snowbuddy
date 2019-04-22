1e9bbe7ad015a0b6854df422d9d975f6b9ec1f18
Adrian Thorburn <adrian.thorburn@gmail.com>
2019-04-22 18:21:26 +1000

    pre nav bar play

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -7,20 +7,4 @@ let navBarToggle = document.getElementById('js-navbar-toggle');
 navBarToggle.addEventListener('click', function () {
   mainNav.classList.toggle('active');
 });
-
-//Home screen video
-// Get the video
-var video = document.getElementById("myVideo");
-// Get the button
-var btn = document.getElementById("myBtn");
-// Pause and play the video, and change the button text
-function myFunction() {
-  if (video.paused) {
-    video.play();
-    btn.innerHTML = "Pause";
-  } else {
-    video.pause();
-    btn.innerHTML = "Play";
-  }
-}
 }
