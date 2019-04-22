398ad58310657484b29f8253a8060fe4001c22f2
Adrian Thorburn <adrian.thorburn@gmail.com>
2019-04-22 11:00:55 +1000

    users table setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_20_081122) do
+ActiveRecord::Schema.define(version: 2019_04_22_010029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2019_04_20_081122) do
     t.integer "lifts"
     t.integer "elevation"
     t.integer "area"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.text "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
