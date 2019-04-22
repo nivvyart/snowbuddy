4e25a71ea0091e6557bc7af4c2b4bee23981a101
Adrian Thorburn <adrian.thorburn@gmail.com>
2019-04-22 18:02:26 +1000

    nav 90%, trying to get background video looping on homepage

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,5 +1,5 @@
 // nav bar JS
-window.onload=function(){
+window.onload = function(){
 
 let mainNav = document.getElementById('js-menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
@@ -7,4 +7,20 @@ let navBarToggle = document.getElementById('js-navbar-toggle');
 navBarToggle.addEventListener('click', function () {
   mainNav.classList.toggle('active');
 });
+
+//Home screen video
+// Get the video
+var video = document.getElementById("myVideo");
+// Get the button
+var btn = document.getElementById("myBtn");
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
 }
