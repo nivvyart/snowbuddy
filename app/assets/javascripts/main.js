4effcd469400ad7cd02086464b6b79be747f61e2
Adrian Thorburn <adrian.thorburn@gmail.com>
2019-04-25 12:20:21 +1000

    resort map styling save point

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,6 +1,7 @@
-// nav bar JS
+//wait for window to load before getting resources
 window.onload = function(){
 
+  // nav bar JS
   let mainNav = document.getElementById('js-menu');
   let navBarToggle = document.getElementById('js-navbar-toggle');
 
@@ -8,25 +9,24 @@ window.onload = function(){
     mainNav.classList.toggle('active');
   });
 
-
   //resort map modal
   // Get the modal
-  var modal = document.getElementById('myModal');
+  let modal = document.getElementById('myModal');
 
-  // Get the image and insert it inside the modal - use its "alt" text as a caption
-  var img = document.getElementById('myImg');
-  var modalImg = document.getElementById("img01");
-  var captionText = document.getElementById("caption");
+  // image inside modal, using alt text as caption 
+  let img = document.getElementById('myImg');
+  let modalImg = document.getElementById("img01");
+  let captionText = document.getElementById("caption");
   img.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.alt;
   }
 
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
+  // close
+  let span = document.getElementsByClassName("close")[0];
 
-  // When the user clicks on <span> (x), close the modal
+  // close
   span.onclick = function() {
     modal.style.display = "none";
   }
